ddd6aa25b4707305c7c4783da05954b3c99e064f
thetnaing <thetnaingoo1530@gmail.com>
2026-01-30 10:28:46 +0630

    integrate with mongodb atlas

--- mongoose.js
+++ mongoose.js
@@ -1,33 +1,40 @@
-// mongoose.js
+
 
 const mongoose = require('mongoose')
-const User = require('./User')
-mongoose.connect("mongodb://localhost/testdb")
-	.then(()=> {
-		console.log('Connected')
-})
-
-// const user = new User({name : "Thet", age : "27"})
-// user.save().then(u => console.log(u, 'is saved'))
 
 
 
-async function run () {
-	try {
-		const user = new User({
-		name : "James Doe",
-		age :"eee",
-		hobbies : ['Weight lifting', 'bowling'],
-		address : {
-			street : 'Natchaung',
-			city : 'Kale'
-		}
-	})
-	await user.save()
-	console.log(user)
-	}catch (e) {
-		console.log(e)
-	}
+if (!process.argv.length > 3) {
+    process.exit(1)
 }
 
-run()
+const password = process.argv[2]
+
+const url = `mongodb+srv://thetnaingoo1530_db_user:${password}@cluster0.owgcwei.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema ({
+    content : String,
+    important : Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//     content : 'mongoose makes things easy',
+//     important : true
+// })
+
+// note.save().then ((n)=> {
+//     console.log(n)
+//     mongoose.connection.close()
+// })
+
+Note.find({})
+    .then((note)=> {
+        note.forEach(n=> {
+            console.log(n)
+        })
+        mongoose.connection.close()
+    })
